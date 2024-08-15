4038dc0707666fd9d3b7543ac5f0272215a69655
Sergey <sinzem@gmail.com>
2024-08-16 00:14:05 +0300

    router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,27 @@
-import { useState } from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import {MainPage, ComicsPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <RandomChar/>   
-                </ErrorBoundary>
-                
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/> 
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/comics">
+                            <ComicsPage />
+                        </Route>
+                        <Route exact path="/"> {/* (главную страницу помещаем последней в списке роутов - ее путь входит в пути всех остальных, это может давать проблему при загрузке страниц(switch и exact делают строгое сравнение)) */}
+                            <MainPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
-
 }
 
 export default App;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -18,6 +18,19 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${PUBLIC_KEY}`
+		);
+		return res.data.results.map(_transformComics);
+	}
+
+	const getComics = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${PUBLIC_KEY}`);
+        return _transformComics(res.data.results[0]);
+    }
+	
+
     const _transformCharacter = (char) => {
         return {
             id: char.id,
@@ -30,12 +43,30 @@ const useMarvelService = () => {
         }
     }
 
+    const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
+
     return {
         loading,
         error,
         getAllCharacters,
         getCharacter,
-        clearError
+        clearError,
+        getAllComics,
+		getComics,
     }
 }
 
