f0748610a81bd5927cffd188a1eac9dccd36739b
Sergey <sinzem@gmail.com>
2024-08-17 00:12:49 +0300

    routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes/* Switch */} from "react-router-dom";
 
 import {MainPage, ComicsPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
@@ -10,14 +10,16 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Switch>
-                        <Route exact path="/comics">
+                    <Routes>{/* <Switch> */} {/* (switch и роуты с дочерними элементами - в 5-й версии react-router-dom) */}
+                        {/* <Route exact path="/comics">
                             <ComicsPage />
                         </Route>
-                        <Route exact path="/"> {/* (главную страницу помещаем последней в списке роутов - ее путь входит в пути всех остальных, это может давать проблему при загрузке страниц(switch и exact делают строгое сравнение)) */}
+                        <Route exact path="/"> (главную страницу помещаем последней в списке роутов - ее путь входит в пути всех остальных, это может давать проблему при загрузке страниц(switch и exact делают строгое сравнение))
                             <MainPage />
-                        </Route>
-                    </Switch>
+                        </Route> */}
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/comics" element={<ComicsPage />} />
+                    </Routes>{/* </Switch> */}
                 </main>
             </div>
         </Router>
