9667c6cb2002316168415da1d01b119f39f49b70
sinzem <111191960+sinzem@users.noreply.github.com>
2025-04-30 22:08:41 +0300

    Update key.js

--- src/env/key.js
+++ src/env/key.js
@@ -1,3 +1,3 @@
-export const PUBLIC_KEY="apikey=efa0073169cbb83c1829af9514156d6c"
+export const PUBLIC_KEY="apikey=efa0073169cbb83c1829af9514156d6c-1"
 
 // https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=efa0073169cbb83c1829af9514156d6c
