03bc2ef295f69276576cf06d45be04acd5ec9d23
sinzem <111191960+sinzem@users.noreply.github.com>
2025-04-30 22:05:14 +0300

    Delete src/env directory

--- src/env/key.js
+++
@@ -1,3 +0,0 @@
-export const PUBLIC_KEY="apikey=efa0073169cbb83c1829af9514156d6c"
-
-// https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=efa0073169cbb83c1829af9514156d6c
