65c14c76055ff9c1d31a3b5284301b0dde3083a9
sinzem <111191960+sinzem@users.noreply.github.com>
2025-04-30 22:01:42 +0300

    Add files via upload

---
+++ src/env/key.js
@@ -0,0 +1,3 @@
+export const PUBLIC_KEY="apikey=efa0073169cbb83c1829af9514156d6c"
+
+// https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=efa0073169cbb83c1829af9514156d6c
